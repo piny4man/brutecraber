36b22dc9a353634c8b71d4fff43462a07d469002
Erik <erikgavs@gmail.com>
2026-03-26 11:47:04 +0100

    error solved

--- src/detector.rs
+++ src/detector.rs
@@ -2,7 +2,7 @@ pub fn detect(hash: &str) -> &str {
     match hash.len() {
         32 => "md5",
         40 => "sha1",
-        64 => "sha156",
+        64 => "sha256",
         128 => "sha512",
         _ => "hash not recognized, try using -t parameter",
     }
