431ab33b123039295e78bdd88875a19b54e889ba
Erik <erikgavs@gmail.com>
2026-03-27 17:31:44 +0100

    cargo test added

--- src/hashes/bcrypt.rs
+++ src/hashes/bcrypt.rs
@@ -4,3 +4,19 @@ use bcrypt::verify;
 pub fn crack(word: &str, hash: &str) -> bool {
     verify(word, hash).unwrap_or(false)
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_crack_valid() {
+        assert!(crack("password", "$2y$04$xa4SIvcnMqIWtM8LZt3/2eoi5AsfZdM.wstwCsGVXDHSVY6Egv9rm"));
+        assert!(crack("admin", "$2y$04$E4S23BqwqDr.QYpl7sHCze61WXTTZbzd3mNZccwd3qHbFsc8yUqhq"));
+    }
+
+    #[test]
+    fn test_crack_invalid() {
+        assert!(!crack("wrongpassword", "$2y$04$xa4SIvcnMqIWtM8LZt3/2eoi5AsfZdM.wstwCsGVXDHSVY6Egv9rm"));
+    }
+}
