b79e10ab635132e6df1cb5f1a0ca0ca82ba8e534
Erik <erikgavs@gmail.com>
2026-03-28 15:25:08 +0100

    ntlm hash cracking logic

--- src/hashes/mod.rs
+++ src/hashes/mod.rs
@@ -1,5 +1,6 @@
 pub mod bcrypt;
 pub mod md5;
+pub mod ntlm;
 pub mod sha1_hash;
 pub mod sha256;
 pub mod sha512;
---
+++ src/hashes/ntlm.rs
@@ -0,0 +1,14 @@
+use md4::{Digest, Md4};
+
+pub fn crack(word: &str) -> String {
+    // Convert to windows encoding
+    let winencode: Vec<u8> = word.encode_utf16().flat_map(|c| c.to_le_bytes()).collect();
+
+    let mut hasher = Md4::new();
+    hasher.update(&winencode);
+    hasher
+        .finalize()  // get hash as raw bytes [93, 65, 64, 42, ...]
+        .iter()      // iterate each byte
+        .map(|b| format!("{:02x}", b))  // convert each byte to hex: 93 -> "5d"
+        .collect()   // join all: "5d41402a..."
+}
