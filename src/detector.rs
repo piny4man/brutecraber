f9ff8d63bb1458cb701868b0c3d966ef0f4258f2
Erik <erikgavs@gmail.com>
2026-04-07 11:31:03 +0200

    feat: detector for sha3

--- src/detector.rs
+++ src/detector.rs
@@ -3,7 +3,7 @@ pub fn detect(hash: &str) -> &str {
         32 => "md5",
         40 => "sha1",
         64 => "sha256/sha3-256",
-        128 => "sha512",
+        128 => "sha512/sha3-512",
         _ => "hash not recognized, try using -t parameter",
     }
 }
