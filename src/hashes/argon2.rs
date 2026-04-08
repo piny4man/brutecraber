c63bacd2756051ef7a1d390fa32ed9e047a77e51
Erik <erikgavs@gmail.com>
2026-04-08 18:43:54 +0200

    more cargo test

--- src/hashes/argon2.rs
+++ src/hashes/argon2.rs
@@ -13,12 +13,12 @@ pub fn verify(word: &str, hash: &str) -> bool {
 mod tests {
     use super::*;
     use argon2::{
-        password_hash::{rand_core::OsRng, PasswordHasher, SaltString},
+        password_hash::{PasswordHasher, SaltString},
         Argon2,
     };
 
-    fn generate_hash(password: &str) -> String {
-        let salt = SaltString::generate(&mut OsRng);
+    fn generate_hash(password: &str, salt: &str) -> String {
+        let salt = SaltString::from_b64(salt).unwrap();
         let argon2 = Argon2::default();
         argon2
             .hash_password(password.as_bytes(), &salt)
@@ -28,19 +28,19 @@ mod tests {
 
     #[test]
     fn test_verify_valid() {
-        let hash = generate_hash("password");
+        let hash = generate_hash("password", "c29tZXNhbHQ");
         assert!(verify("password", &hash));
     }
 
     #[test]
     fn test_verify_invalid() {
-        let hash = generate_hash("password");
+        let hash = generate_hash("password", "c29tZXNhbHQ");
         assert!(!verify("wrongpassword", &hash));
     }
 
     #[test]
     fn test_verify_admin() {
-        let hash = generate_hash("admin");
+        let hash = generate_hash("admin", "YW5vdGhlcnNhbHQ");
         assert!(verify("admin", &hash));
         assert!(!verify("administrator", &hash));
     }
